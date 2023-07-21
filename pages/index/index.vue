02af0d33ddc5fa01b2b534091f122489390ac7d8
xavi <xaviw@foxmail.com>
2023-07-21 23:41:43 +0800

    fix: webview组件文件名错误修改

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -5,12 +5,11 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {}
-    },
-  }
-
+export default {
+  data() {
+    return {}
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
